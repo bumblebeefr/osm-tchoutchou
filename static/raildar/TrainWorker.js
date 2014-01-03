d831619a3e72045e1fb393ce4420b776009e5f3a
fd <fd@themetricsfactory.com>
2014-01-03 02:20:21 +0100

    First TrainWorker step : the worker now execute the ajax requests.

--- static/raildar/TrainWorker.js
+++ static/raildar/TrainWorker.js
@@ -1,76 +1,90 @@
-var window = {
-		location :"",
-		document : {
-			documentElement : {},
-			createElement : function(){}
+var worker = this;
+
+importScripts('./Train.js', '../moment.min.js', '../underscore-min.js');
+
+var getJSON = function(url, options) {
+	if (options == null) {
+		options = {};
+	}
+	if (!('data' in options) || options.data == undefined) {
+		options.data = {};
+	}
+	options.data['_'] = (new Date()).getTime();
+	for (k in options.data) {
+		url += (url.indexOf("?") == -1) ? "?" : "&";
+		url += encodeURIComponent(k);
+		url += "=";
+		url += encodeURIComponent(options.data[k]);
+	}
+
+	var req = new XMLHttpRequest();
+	try{
+		req.open('GET', url, true);
+		req.responseType = "json";
+		req.onreadystatechange = function(aEvt) {
+			if (req.readyState == 4) {
+				if (req.status == 200) {
+					if(typeof options.success == 'function'){
+						options.success(req.response,req.statusText,req);
+					}
+				} else {
+					if(typeof options.error == 'function'){
+						options.error(req.statusText,req,null);
+					}
+				}
+				if(typeof options.complete == 'function'){
+					options.complete(req,req.statusText);
+				}
+			}
+		};
+		req.send(null);
+	}catch(e){
+		if(typeof options.error == 'function'){
+			options.error(req.statusText, req,e);
 		}
-};
-var document = window.document;
-
-importScripts('./Train.js','../moment.min.js','../underscore-min.js');
-
-var train = {
-	"geometry" : {
-		"coordinates" : [ 2.4667, 49.1679 ],
-		"type" : "Point"
-	},
-	"type" : "Feature",
-	"properties" : {
-		"next_gare" : "Compiegne",
-		"id_mission" : "154696",
-		"num" : "2315",
-		"last_update" : "2014-01-02 17:54:06",
-		"heading" : "145",
-		"brand" : "Intercite",
-		"retard" : "0",
-		"minutes_to_next_gare" : "20",
-		"pos_type" : "1",
-		"id_next_gare" : "1271",
-		"last_check" : "2014-01-02 17:51:01",
-		"terminus" : "Cambrai Ville",
-		"id_terminus" : "1077"
+		console.error(e);
 	}
 };
+
 var handlers = {
 	updateFilters : function(options) {
-		
+
 	},
-	
-	//options.params : parametre de la requete ajax
-	get_circulation : function(options){
-//		jQuery.ajax("http://www.raildar.fr/json/get_circulation.json",{
-//			async : true,
-//			cache : false,
-//			data : options.params,	
-//			error : function(jqXHR,textStatus,errorThrown){
-//				this.postMessage({
-//					type : "error",
-//					data : {
-//						'jqXHR' : jqXHR,
-//						'textStatus' : textStatus,
-//						'errorThrown' : errorThrown
-//					}
-//				});
-//			},
-//			success : function(data, textStatus, jqXHR){
-//				this.postMessage({
-//					type : "circulation",
-//					data : data
-//				});
-//				
-//			}
-//		});
-	}	
+
+	// options.params : parametre de la requete ajax
+	get_circulation : function(options) {
+		 getJSON("http://www.raildar.fr/json/get_circulation.json", {
+			data : options.data,
+			error : function(jqXHR, textStatus, errorThrown) {
+				worker.postMessage({
+					type : "circulation_error",
+					data : {
+						'textStatus' : textStatus,
+						'errorThrown' : errorThrown.toString()
+					}
+				});
+			},
+			success : function(data, textStatus, jqXHR) {
+				worker.postMessage({
+					type : "circulation_success",
+					data : data
+				});
+
+			},
+			complete : function(jqXHR, textStatus) {
+				worker.postMessage({
+					type : "circulation_complete",
+					data : {textStatus : textStatus}
+				});
+
+			}
+		});
+	}
 };
 this.addEventListener('message', function(e) {
-	console.log(e.data.cmd);
-//	var t = new Train(train);
-//	this.postMessage({
-//		train : t
-//	});
-	if(e.data.cmd in handlers){
+	if (e.data.cmd in handlers) {
 		handlers[e.data.cmd](e.data.parameter);
-	}else{
-		console.error("No handler defined for cmd "+e.cmd);
+	} else {
+		console.error("No handler defined for cmd " + e.cmd);
 	}
 }, false);
