03eacd87041e9d97cd9ad66aecadb54e53305ad7
mac <mac@maccall.fr>
2014-01-16 21:37:48 +0100

    config des couches principales de trains

--- static/raildar/datasources/DataSourceConfig.js
+++ static/raildar/datasources/DataSourceConfig.js
@@ -3,7 +3,7 @@ var DataSourceConfig = {
 	'national' : new TrainDataSource({
 		'url' : 'http://raildar.fr/json/get_circulation',
 		'urlParams' : {
-			'source' : 'garesenmouvement'
+			'id_source' : '1'
 		},
 		'center' : {
 			'lat' : 46.81,
@@ -13,17 +13,47 @@ var DataSourceConfig = {
 		'maxZoom' : null,
 		'refreshDelay' : 10*1000
 	}),
-//	'regional' : {
+	'regional' : new TrainDataSource({
+		'url' : 'http://raildar.fr/json/get_circulation',
+		'urlParams' : {
+			'id_source' : '2'
+		},
+		'center' : {
+			'lat' : 46.81,
+			'lng' : 6.88
+		},
+		'minZoom' : 5,
+		'maxZoom' : null,
+		'refreshDelay' : 10*1000
+	}),
 
-//
-//	},
-//	'idf' : {
 
-//
-//	},
-//	'toulouse' : {
-
-//	},
+	'idf' : new TrainDataSource({
+		'url' : 'http://raildar.fr/json/get_circulation',
+		'urlParams' : {
+			'id_source' : '3'
+		},
+		'center' : {
+			'lat' : 48.854,
+			'lng' : 2.348
+		},
+		'minZoom' : 7,
+		'maxZoom' : null,
+		'refreshDelay' : 10*1000
+	}),
+	'toulouse'  : new TrainDataSource({
+		'url' : 'http://raildar.fr/json/get_circulation',
+		'urlParams' : {
+			'id_source' : '4'
+		},
+		'center' : {
+			'lat' : 43.621,
+			'lng' : 1.472
+		},
+		'minZoom' : 7,
+		'maxZoom' : null,
+		'refreshDelay' : 10*1000
+	}),
 //	'stats' : {
 //
 //	}
